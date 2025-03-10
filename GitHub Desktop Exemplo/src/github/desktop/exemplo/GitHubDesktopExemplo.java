82384add69069c675c084f6a6ad1d0197e90b5a8
Jhunyor007 <aurelio.jhunior@gmail.com>
2025-03-10 19:36:53 -0300

    Hello  World

--- GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
+++ GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -1,20 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package github.desktop.exemplo;
 
-/**
- *
- * @author aurel
- */
 public class GitHubDesktopExemplo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Sem Bem-Vindo Aurélio Júnior");
+        System.out.println("Curso TDS-Estamos chegando");
     }
     
 }
